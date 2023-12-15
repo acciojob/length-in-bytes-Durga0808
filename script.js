21571d51ca345428b3283ac6942222fb2f184968
Durga Prasad Bejawada <117895509+Durga0808@users.noreply.github.com>
2023-12-15 18:22:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 const byteSize = (str) => {
   // write your code here
-	return str.length;
+	const blob = new Blob([str]);
+
+  // Get the size of the Blob in bytes
+  const bytes = blob.size;
+
+  return bytes;
 };
 
 // Do not change the code below
